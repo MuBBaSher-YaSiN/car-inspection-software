1f9c6732563846153566e46b46020f67b2bbebca
Fatima_Iqra <iqraf683@gmail.com>
2025-07-31 20:50:52 +0500

    Add Redux Toolkit setup with auth and job slices

---
+++ car-inspection/src/redux/slices/jobSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface Job {
+  _id: string;
+  carNumber: string;
+  customerName: string;
+  status: string;
+  [key: string]: any;
+}
+
+interface JobState {
+  jobs: Job[];
+}
+
+const initialState: JobState = {
+  jobs: [],
+};
+
+export const jobSlice = createSlice({
+  name: 'jobs',
+  initialState,
+  reducers: {
+    setJobs(state, action: PayloadAction<Job[]>) {
+      state.jobs = action.payload;
+    },
+    addJob(state, action: PayloadAction<Job>) {
+      state.jobs.unshift(action.payload);
+    },
+    updateJob(state, action: PayloadAction<Job>) {
+      const index = state.jobs.findIndex((j) => j._id === action.payload._id);
+      if (index !== -1) {
+        state.jobs[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { setJobs, addJob, updateJob } = jobSlice.actions;
+export default jobSlice.reducer;
