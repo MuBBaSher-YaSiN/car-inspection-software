8d18eac607e01f159db95c1e99aa40ba1f9e83d9
Fatima_Iqra <iqraf683@gmail.com>
2025-08-04 18:43:05 +0500

    update

--- car-inspection/src/app/layout.tsx
+++ car-inspection/src/app/layout.tsx
@@ -4,15 +4,18 @@ import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "@/redux/store";
+import { ThemeProvider } from "@/components/theme-provider"; 
 import './globals.css';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <SessionProvider>
           <ReduxProvider store={store}>
-            {children}
+            <ThemeProvider> {/*wrapping app with theme provider */}
+              {children}
+            </ThemeProvider>
           </ReduxProvider>
         </SessionProvider>
       </body>
